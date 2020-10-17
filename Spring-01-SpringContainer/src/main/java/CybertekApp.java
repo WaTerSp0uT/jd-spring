a3304f11450974504b8bef11f37e1b20151ebb9a
WaTerSp0uT <ericyarek@gmail.com>
2020-10-17 14:47:32 -0400

    Calling bean via ApplicationContext without downCasting via Overloading Method

--- Spring-01-SpringContainer/src/main/java/CybertekApp.java
+++ Spring-01-SpringContainer/src/main/java/CybertekApp.java
@@ -8,11 +8,14 @@ public class CybertekApp {
     public static void main(String[] args) {
 
         //BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
-        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
-        Mentor mentor = (Mentor) container.getBean("fullTimeMentor");
-        mentor.createAccount();
+//        Mentor mentor = (Mentor) container.getBean("fullTimeMentor");
+//        mentor.createAccount();
+//
+//        Mentor mentor1 = (Mentor) container.getBean("partTimeMentor");
+//        mentor1.createAccount();
 
-        Mentor mentor1 = (Mentor) container.getBean("partTimeMentor");
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        Mentor mentor1 = container.getBean("partTimeMentor", Mentor.class);
         mentor1.createAccount();
     }
 }
