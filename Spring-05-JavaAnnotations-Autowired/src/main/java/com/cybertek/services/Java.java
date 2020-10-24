7dd04cd17b4f7751cea4a75c5d5a6ea0af6d269a
WaTerSp0uT <ericyarek@gmail.com>
2020-10-24 11:33:44 -0400

    Example with @Autowired

--- Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
@@ -1,11 +1,17 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
     private OfficeHours officeHours;
+
+    @Autowired
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
 
     @Override
     public void getTeachingHours() {
