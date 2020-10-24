6dd5b3d342f7b7b698cdd2802ef8451e28d4ba8b
WaTerSp0uT <ericyarek@gmail.com>
2020-10-24 11:21:40 -0400

    Create new module 05, for Java Annotations Autowired

--- Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Selenium.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Selenium.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Component;
 public class Selenium implements Course {
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours: 30");
+        System.out.println("Weekly teaching hours: 50");
     }
 }
