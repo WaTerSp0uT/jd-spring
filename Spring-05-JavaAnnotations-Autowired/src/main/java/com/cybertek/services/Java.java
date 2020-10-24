b077fdd4622728ea323c437e5d11b956cc99da66
WaTerSp0uT <ericyarek@gmail.com>
2020-10-24 11:38:21 -0400

    Example with Filed @Autowired

--- Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
@@ -4,8 +4,13 @@ import com.cybertek.interfaces.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.lang.annotation.Target;
+
 @Component
 public class Java implements Course {
+
+    //Field Injection
+    @Autowired
     private OfficeHours officeHours;
 
     //Constructor Injection
@@ -14,10 +19,12 @@ public class Java implements Course {
     //    this.officeHours = officeHours;
     //}
 
-    @Autowired
-    public void setOfficeHours(OfficeHours officeHours) {
-        this.officeHours = officeHours;
-    }
+    //Setter Injection
+//    @Autowired
+//    public void setOfficeHours(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
 
     @Override
     public void getTeachingHours() {
