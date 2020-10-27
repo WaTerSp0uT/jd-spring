a035b78d24c33b6df15e9516dc378f57ddd1581d
WaTerSp0uT <ericyarek@gmail.com>
2020-10-27 19:35:19 -0400

    PreDestroy Postdestroy

---
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/cybertek/CybertekApp.java
@@ -0,0 +1,22 @@
+package cybertek;
+
+
+import cybertek.configs.CybertekAppConfig;
+import cybertek.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(CybertekAppConfig.class);
+
+        Course course = container.getBean("java",Course.class);
+
+        course.getTeachingHours();
+
+        ((AnnotationConfigApplicationContext)container).close();
+
+
+    }
+}
---
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/cybertek/configs/CybertekAppConfig.java
@@ -0,0 +1,14 @@
+package cybertek.configs;
+
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("cybertek")
+public class CybertekAppConfig {
+
+
+}
