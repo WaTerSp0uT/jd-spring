93440d53eb32b64ea08715ea1d8e8f7edee83254
WaTerSp0uT <ericyarek@gmail.com>
2020-11-24 21:38:13 -0500

    Added confirmation page after filling the HTML page

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -4,6 +4,7 @@ import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.lang.reflect.Array;
@@ -24,6 +25,11 @@ public class MentorController {
 
 
         return "mentor/mentor-register";
+    }
+    @PostMapping("/confirm")
+    public String submitForm(){
+
+        return "mentor/mentor-confirmation";
     }
 
 }
