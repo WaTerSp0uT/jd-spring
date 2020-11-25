d61a8b38548a8fec92bf64dcf153b1c8b7818680
WaTerSp0uT <ericyarek@gmail.com>
2020-11-24 21:16:49 -0500

    HTML form binded

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -1,9 +1,14 @@
 package com.cybertek.controller;
 
+import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
@@ -11,6 +16,12 @@ public class MentorController {
 
     @GetMapping("/register")
     public String showForm(Model model){
+
+        model.addAttribute("mentor", new Mentor());
+
+        List<String> batchList = Arrays.asList("B7","B8","B9","E1","E2","E3","J7","J17","J27","J47");
+        model.addAttribute("batchList",batchList);
+
 
         return "mentor/mentor-register";
     }
